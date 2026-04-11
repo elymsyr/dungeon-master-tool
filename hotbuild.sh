34befdac2744740ed91af64c7bbc878c25a98af8
Orhun Eren Yalçınkaya <orhunerenyalcinkaya@gmail.com>
2026-04-11 23:19:40 +0300

    update

--- hotbuild.sh
+++
@@ -1,8 +0,0 @@
-cd flutter_app
-
-flutter build apk --debug
-adb install -r build/app/outputs/flutter-apk/app-debug.apk
-
-adb logcat | grep -E "ScreencastPlugin|SCREENCAST"
-
-export PATH="/home/eren/.flutter-sdk/bin:$PATH" && cd /home/eren/GitHub/dungeon-master-tool/flutter_app && flutter run -d linux
