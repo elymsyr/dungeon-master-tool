e22f703950c0a6f38c9d3973e9a5d724c997408c
Orhun Eren Yalçınkaya <orhunerenyalcinkaya@gmail.com>
2026-04-13 15:00:27 +0300

    update

--- flutter_app/lib/core/constants.dart
+++ flutter_app/lib/core/constants.dart
@@ -1,5 +1,5 @@
 const String appName = 'Dungeon Master Tool';
-const String appVersion = '4.1.0';
+const String appVersion = '4.2.0';
 const String appProcess = 'beta';
 String get appReleaseTag => '$appProcess-v$appVersion';
 const String githubRepo = 'elymsyr/dungeon-master-tool';
